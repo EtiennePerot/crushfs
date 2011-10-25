13d753cf0271e33acf6e2f5b13b90ac480b51164
Etienne Perot <etienne@perot.me>
2011-10-25 16:37:08 -0400

    Allow both arguments

--- crushfs.py
+++ crushfs.py
@@ -78,7 +78,7 @@ class crushfs(callbackfs.callbackfs):
 		self.addCallback(r'\.jpe?g$', JPEGCrusher)
 
 if __name__ == '__main__':
-	if len(sys.argv) not in (3, 4):
+	if len(sys.argv) < 3:
 		print('usage: %s [--enqueue] <backing directory> <mountpoint>' % sys.argv[0])
 		sys.exit(1)
 	enqueue = '--enqueue' in sys.argv
